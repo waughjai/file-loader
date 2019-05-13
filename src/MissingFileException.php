2cf9831bf449b13eea94389ad1048f8e5db97add
Jaimeson Waugh <jaimeson@4cesi.com>
2019-05-13 16:20:46 -0700

    Make MissingFileException mo' generic 'bout what types o' variables it allows

--- src/MissingFileException.php
+++ src/MissingFileException.php
@@ -5,19 +5,21 @@ namespace WaughJ\FileLoader
 {
 	class MissingFileException extends \RuntimeException
 	{
-		public function __construct( string $filename, string $fallback )
+		public function __construct( $filename, $fallback )
 		{
 			$this->filename = $filename;
 			$this->fallback = $fallback;
-			parent::__construct( "Error: could not find modified time for file \"{$filename}\"" );
+
+			$message_filename = ( is_array( $filename ) && !empty( $filename ) ) ? $filename[ 0 ] : ( string )( $filename );
+			parent::__construct( "Error: could not find modified time for file \"{$message_filename}\"" );
 		}
 
-		public function getFilename() : string
+		public function getFilename()
 		{
 			return $this->filename;
 		}
 
-		public function getFallbackContent() : string
+		public function getFallbackContent()
 		{
 			return $this->fallback;
 		}
